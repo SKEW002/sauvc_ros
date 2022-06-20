0fa97500834692cc18b6d872c2aead24fa652f66
SY <skew002@e.ntu.edu.sg>
2022-06-20 20:34:59 +0800

    added arduino control

---
+++ auv_arduino/auv_arduino.ino
@@ -0,0 +1,27 @@
+#include <ros.h>
+#include <std_msgs/Bool.h>
+#include <std_msgs/Float32.h>
+#include <geometry_msgs/Vector3.h>
+#include "sensor_msgs/Imu.h"
+
+ros::NodeHandle nh;
+
+//void imuCallback(const sensor_msgs::Imu::ConstPtr& msg) {
+//  float ox = msg->orientation.x;
+//  float oy = msg->orientation.y;
+//  float oz = msg->orientation.z;
+//  float ow = msg->orientation.w;
+//
+//}
+//ros::Subscriber<std_msgs::Float32> imu_sub("/zedm/zed_node/imu/data", &imuCallback);
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
